006014920324e3309b29db2881b6f67906aa7c12
Felipe Kennedy <felipekennedyfvs2019@gmail.com>
2024-05-22 17:17:42 -0300

    finalmente variáveis

---
+++ Aulas/aula6.py
@@ -0,0 +1,18 @@
+# conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+# tipos imutaveis e primitivos:
+# str, int, float, bool
+
+print(1 + 1) # =2
+print("a" + "b") # = ab (Concatenação)
+# print("a" + 2) # = erro tipos diferentes
+print("a" + str(2)) # = a2 (Conversão de tipos)
+print(bool('')) # = converte pra booleano, vazia é False
+print(bool(' ')) # = converte pra booleano, e com qualquer outro valor é True
+
+
+
+#(extra do auto complete)
+# tipos mutaveis e complexos:
+# list, tuple, dict, set, frozenset, range
---
+++ Aulas/aula7.py
@@ -0,0 +1,19 @@
+# Variaveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# némeros e underline _.
+# O sinal de = é o operador de atribuição. ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+# nome_completo = 'Felipe Kennedy Alencar Mesquita'
+# soma_dois_mais_dois = 2 + 2
+# int_um = int('1')
+# bool_1 = bool('1')
+# print(int_um, type(int_um),bool_1, type(bool_1) )
+# print(nome_completo, soma_dois_mais_dois) # Felipe Kennedy Alencar Mesquita, 4
+
+nome = 'Felipe'
+idade = 28
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
