b1dcdf426bd163b9ef21adb9c798629a5d99d996
Felipe Kennedy <felipekennedyfvs2019@gmail.com>
2024-05-22 17:48:36 -0300

    outro dia de curso
    cursinho Udemy

---
+++ Aulas/aula8.py
@@ -0,0 +1,13 @@
+nome = 'Felipe Kennedy'
+sobrenome = 'Alencar Mesquita'
+idade = 28
+ano_de_nascimento = 1996
+e_maior_de_idade = 2024 - ano_de_nascimento >=18
+altura_em_metros = 1.69
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento:', ano_de_nascimento)
+print('É maior de idade:', e_maior_de_idade)
+print('Altura em metros:', altura_em_metros)
---
+++ Aulas/aula9.py
@@ -0,0 +1,22 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2 # essa divisão sempre retorna um float não importa o que
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2 # e essa nem sempre retorna um float, sempre corta as casas decimais
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2 # resto da divisão
+print('Módulo', modulo)
+print(10 % 8 == 0) # dez é multiplo de 8? se  o resto da divisão for 0, é multiplo, se não não é
+print(16 % 8 == 0) 
