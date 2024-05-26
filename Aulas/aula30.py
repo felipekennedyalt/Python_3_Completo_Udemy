31f317ad7bb3d8d3a49b4753f3f2b6cfcf149e57
Felipe Kennedy <felipekennedyfvs2019@gmail.com>
2024-05-26 17:01:09 -0300

    curso
    estou acidentado e todo dolorido

--- Aulas/aula30.py
+++ Aulas/aula30.py
@@ -3,10 +3,23 @@ CONSTANTE = "Variáveis" que não vao mudar
 Muitas condições no mesmo if (ruim)
     <- Contagem de complexidade (ruim)
 """
-Velocidade = 61 # velocidade atual do carro
-local_carro = 90 # local em que o carro está na estrada
+velocidade = 61 # velocidade atual do carro
+local_carro = 100 # local em que o carro está na estrada
 
 RADAR_1 = 60 # velocidade máxima do radar 1
 LOCAL_1 = 100 # local onde o radar 1 está 
 RADAR_RANGE = 1 # A distaância onde o radar pega
 
+velocidade_carro_ao_passar_radar1 = velocidade > RADAR_1
+carro_passsou_radar1 = local_carro >= (LOCAL_1 - RADAR_RANGE) and local_carro <= (LOCAL_1 + RADAR_RANGE)
+carro_multado_radar_1 = carro_passsou_radar1 and velocidade_carro_ao_passar_radar1
+
+print(velocidade_carro_ao_passar_radar1)
+print(carro_passsou_radar1)
+
+if velocidade_carro_ao_passar_radar1:
+    print("Você está acima da velocidade permitida no radar 1")
+
+if carro_multado_radar_1:
+    print("Carro multado em Radar 1")
+    
---
+++ Aulas/aula31.py
@@ -0,0 +1,20 @@
+"""
+Flag (Bandeira) - Marcar um local
+None = não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = Identidade
+"""
+
+v1 = 'a'
+v2 = 'a'
+v3 = 'b'
+print(id(v1))
+print(id(v2))
+print(id(v3))
+
+# condcao = False
+
+#if condicao:
+#    print('faça algo')
+# else:
+#    print('não faça nada')
